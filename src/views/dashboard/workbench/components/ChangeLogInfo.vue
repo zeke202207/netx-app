f00f134787c2954c7075a73f1e5b8bacb5530510
zeke <mo1986@126.com>
2022-10-30 14:14:34 +0800

    首页添加更新日志功能

---
+++ src/views/dashboard/workbench/components/ChangeLogInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <Card title="更新日志" v-bind="$attrs">
+    <Timeline>
+      <Timeline.Item v-for="logitem in changeLogInfoItems">
+        <p>{{ logitem.version }}</p>
+        <p>{{ logitem.changedate }}</p>
+        <ul v-for="item in logitem.content">
+          <li style="padding-left: 30px">
+            <SvgIcon style="margin-right: 10px" :name="item.iconname" />
+            {{ item.info }}
+          </li>
+        </ul>
+      </Timeline.Item>
+    </Timeline>
+  </Card>
+</template>
+<script lang="ts" setup>
+  import { Card, Timeline } from 'ant-design-vue';
+  import { changeLogInfoItems } from './data';
+  import { SvgIcon } from '/@/components/Icon';
+</script>
